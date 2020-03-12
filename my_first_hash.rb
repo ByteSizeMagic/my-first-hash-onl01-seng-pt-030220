ef4ba8efe3aaf78a750bf9439bce634875d7164a
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-12 11:43:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_car = Hash.new
-  my_car["color"] = "gray"
+  my_hash = {"full_name" => "Crystal Schooling", "favorite_food" => "tacos"}
 end
 
 
