84ba5e8abb71c9161f29b298334b3be915dfc8df
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-12 11:37:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_car = Hash.new
-  my_car[color] = "gray"
+  my_car["color"] = "gray"
 end
 
 
